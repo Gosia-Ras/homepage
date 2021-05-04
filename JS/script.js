7cb1a92d4bd678926e9aa8b39430868ef327ac04
Malgorzata Ras <ras.malgorzata@gmail.com>
2021-05-04 16:13:15 +0200

    empty lines and white spaces removed

--- JS/script.js
+++ JS/script.js
@@ -3,31 +3,18 @@
     const hidePhotoButton = document.querySelector(".js-buttonHideImage");
 
     const showPhotosContainer = () => {
-
         const imagesContainer = document.querySelector(".js-imagesContainer");
-
         imagesContainer.classList.toggle("imagesContainer--hidden");
-
         containerButton.innerText = imagesContainer.classList.contains("imagesContainer--hidden") ? "Hide photos" : "Show photos";
-        
-
     }
 
-
-
     const hideIntroductionPhoto = () => {
-
         const image = document.querySelector(".js-image");
-
         image.classList.toggle("image--hidden");
         hidePhotoButton.innerText = image.classList.contains("image--hidden") ? "Show photo" : "Hide photo";
-
-
     };
 
     const init = () => {
-
-    
         hidePhotoButton.addEventListener("click", hideIntroductionPhoto);
         containerButton.addEventListener("click", showPhotosContainer);
     };
