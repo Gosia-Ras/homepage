8bb696d4dc1d54d44fb6c41f28d8b7df7e124e4f
Malgorzata Ras <ras.malgorzata@gmail.com>
2021-05-04 16:31:44 +0200

    replaced function "init" with 2 functions

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,11 @@
 {
     const containerButton = document.querySelector(".js-buttonHideContainer");
-    const hidePhotoButton = document.querySelector(".js-buttonHideImage");
 
     const showPhotosContainer = () => {
         const imagesContainer = document.querySelector(".js-imagesContainer");
         imagesContainer.classList.toggle("imagesContainer--hidden");
         containerButton.innerText = imagesContainer.classList.contains("imagesContainer--hidden") ? "Hide photos" : "Show photos";
-    }
+    };
 
     const hideIntroductionPhoto = () => {
         const image = document.querySelector(".js-image");
@@ -14,10 +13,16 @@
         hidePhotoButton.innerText = image.classList.contains("image--hidden") ? "Show photo" : "Hide photo";
     };
 
-    const init = () => {
+    const introPhotoHide = () => {
+        const hidePhotoButton = document.querySelector(".js-buttonHideImage");
         hidePhotoButton.addEventListener("click", hideIntroductionPhoto);
+    };
+
+    const photosShow = () => {
+        const containerButton = document.querySelector(".js-buttonHideContainer");
         containerButton.addEventListener("click", showPhotosContainer);
     };
 
-    init();
+    introPhotoHide();
+    photosShow();
 };
