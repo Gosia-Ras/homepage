6738b1e473a2797a4dbc4fe0d28da63ce7fc0973
Malgorzata Ras <ras.malgorzata@gmail.com>
2021-05-14 18:17:57 +0200

    corrected errors in js

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,25 @@
 {
-    const toggleDarkMode = () => {
-        const darkMode = document.querySelector(".js-darkMode");
-        
-        darkMode.classList.toggle("body--dark");
-        buttonDark.innerText = darkMode.classList.contains("body--dark") ? "Dark mode off" : "Dark mode on";
+    const hidePhotoButton = document.querySelector(".js-buttonHideImage");
+    const buttonDark = document.querySelector(".js-onDarkButton");
+    const containerButton = document.querySelector(".js-buttonHideContainer");
+
+    const introPhotoHide = () => {
+        hidePhotoButton.addEventListener("click", hideIntroductionPhoto);
     };
 
     const onDarkMode = () => {
-        const buttonDark = document.querySelector(".js-onDarkButton");
         buttonDark.addEventListener("click", toggleDarkMode);
-    }
+    };
+
+    const photosShow = () => {
+        containerButton.addEventListener("click", showPhotosContainer);
+    };
+
+    const toggleDarkMode = () => {
+        const darkMode = document.querySelector(".js-darkMode");
+        darkMode.classList.toggle("body--dark");
+        buttonDark.innerText = darkMode.classList.contains("body--dark") ? "Dark mode off" : "Dark mode on";
+    };
 
     const showPhotosContainer = () => {
         const imagesContainer = document.querySelector(".js-imagesContainer");
@@ -21,16 +31,6 @@
         const image = document.querySelector(".js-image");
         image.classList.toggle("image--hidden");
         hidePhotoButton.innerText = image.classList.contains("image--hidden") ? "Show photo" : "Hide photo";
-    };
-
-    const introPhotoHide = () => {
-        const hidePhotoButton = document.querySelector(".js-buttonHideImage");
-        hidePhotoButton.addEventListener("click", hideIntroductionPhoto);
-    };
-
-    const photosShow = () => {
-        const containerButton = document.querySelector(".js-buttonHideContainer");
-        containerButton.addEventListener("click", showPhotosContainer);
     };
 
     introPhotoHide();
