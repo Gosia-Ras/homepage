56c1848f3298881aacf2f66f5db3cf17364612cd
Gosia Ras <ras.malgorzata@gmail.com>
2021-10-13 17:56:39 +0200

    hide photo button removed

--- JS/script.js
+++ JS/script.js
@@ -1,28 +1,17 @@
 {
-    const hidePhotoButton = document.querySelector(".js-buttonHideImage");
-    const buttonDark = document.querySelector(".js-onDarkButton");
-    
+  const buttonDark = document.querySelector(".js-onDarkButton");
 
-    const introPhotoHide = () => {
-        hidePhotoButton.addEventListener("click", hideIntroductionPhoto);
-    };
+  const onDarkMode = () => {
+    buttonDark.addEventListener("click", toggleDarkMode);
+  };
 
-    const onDarkMode = () => {
-        buttonDark.addEventListener("click", toggleDarkMode);
-    }; 
+  const toggleDarkMode = () => {
+    const darkMode = document.querySelector(".js-darkMode");
+    darkMode.classList.toggle("body--dark");
+    buttonDark.innerText = darkMode.classList.contains("body--dark")
+      ? "Dark mode off"
+      : "Dark mode on";
+  };
 
-    const toggleDarkMode = () => {
-        const darkMode = document.querySelector(".js-darkMode");
-        darkMode.classList.toggle("body--dark");
-        buttonDark.innerText = darkMode.classList.contains("body--dark") ? "Dark mode off" : "Dark mode on";
-    };
-
-    const hideIntroductionPhoto = () => {
-        const image = document.querySelector(".js-image");
-        image.classList.toggle("image--hidden");
-        hidePhotoButton.innerText = image.classList.contains("image--hidden") ? "Show photo" : "Hide photo";
-    };
-
-    introPhotoHide();
-    onDarkMode();
-};
+  onDarkMode();
+}
