9536be7bf825b052ca8c0f42e5d90ac068803ba9
Gosia Ras <ras.malgorzata@gmail.com>
2021-10-13 17:14:48 +0200

    redundant scripts removed from page

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,7 @@
 {
     const hidePhotoButton = document.querySelector(".js-buttonHideImage");
     const buttonDark = document.querySelector(".js-onDarkButton");
-    const containerButton = document.querySelector(".js-buttonHideContainer");
+    
 
     const introPhotoHide = () => {
         hidePhotoButton.addEventListener("click", hideIntroductionPhoto);
@@ -9,22 +9,12 @@
 
     const onDarkMode = () => {
         buttonDark.addEventListener("click", toggleDarkMode);
-    };
-
-    const photosShow = () => {
-        containerButton.addEventListener("click", showPhotosContainer);
-    };
+    }; 
 
     const toggleDarkMode = () => {
         const darkMode = document.querySelector(".js-darkMode");
         darkMode.classList.toggle("body--dark");
         buttonDark.innerText = darkMode.classList.contains("body--dark") ? "Dark mode off" : "Dark mode on";
-    };
-
-    const showPhotosContainer = () => {
-        const imagesContainer = document.querySelector(".js-imagesContainer");
-        imagesContainer.classList.toggle("imagesContainer--hidden");
-        containerButton.innerText = imagesContainer.classList.contains("imagesContainer--hidden") ? "Hide photos" : "Show photos";
     };
 
     const hideIntroductionPhoto = () => {
@@ -34,6 +24,5 @@
     };
 
     introPhotoHide();
-    photosShow();
     onDarkMode();
 };
